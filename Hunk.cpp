7d6156f2055589433c30308def16510b33471bf7
Gregory S. Read <18175637+readgs@users.noreply.github.com>
2003-06-21 07:00:20 +0000

    -Use DiffFile_xxx enums instead of numbers for array indexes.  This makes the code clearer. -Started implementing Replace method (incomplete).

--- Hunk.cpp
+++ Hunk.cpp
@@ -9,7 +9,7 @@
 using namespace MojoMerge;
 
 Hunk::Hunk(Hunk *Prev, uint32 Start1, uint32 End1, uint32 Start2, uint32 End2,
-    uint32 Start3, uint32 End3)
+    uint32 Start3, uint32 End3, DiffFileNumber FileThatIsDifferent)
 {
     // Initialize default values for member variables
     this->Prev = Prev;
@@ -22,12 +22,14 @@ Hunk::Hunk(Hunk *Prev, uint32 Start1, uint32 End1, uint32 Start2, uint32 End2,
     // Check the validity of the hunks
     assert(CheckValidity());
     // Set start and end of each hunk
-    Start[0] = Start1;
-    Start[1] = Start2;
-    Start[2] = Start3;
-    End[0] = End1;
-    End[1] = End2;
-    End[2] = End3;
+    Start[DiffFile_One] = Start1;
+    Start[DiffFile_Two] = Start2;
+    Start[DiffFile_Three] = Start3;
+    End[DiffFile_One] = End1;
+    End[DiffFile_Two] = End2;
+    End[DiffFile_Three] = End3;
+    // Save the file that is different
+    this->FileThatIsDifferent = FileThatIsDifferent;
 }
 
 Hunk::~Hunk()
@@ -43,13 +45,19 @@ Hunk::~Hunk()
 uint32 Hunk::GetStart(DiffFileNumber FileNumber)
 {
     // Return the starting line number of specified file
-    return Start[FileNumber - 1];
+    return Start[FileNumber];
 }
 
 uint32 Hunk::GetEnd(DiffFileNumber FileNumber)
 {
     // Return the ending line number of specified file
-    return End[FileNumber - 1];
+    return End[FileNumber];
+}
+
+DiffFileNumber Hunk::GetDiffFile()
+{
+    // Return the file that is different, if applicable
+    return FileThatIsDifferent;
 }
 
 Hunk *Hunk::GetPrevHunk()
@@ -67,7 +75,7 @@ bool Hunk::CheckValidity()
     uint32 i;
 
     // TODO - Add more checks for Hunk validity
-    for(i = 0; i < 3; i++)
+    for(i = DiffFile_One; i <= DiffFile_Three; i++)
     {
         // UNSPECIFIED is not valid for 1st and 2nd files
         if(i != DiffFile_Three && Start[i] == UNSPECIFIED)
@@ -84,4 +92,27 @@ void Hunk::DeleteList()
         this->Next->DeleteList();
     // Delete this hunk...duh.
     delete this;
+}
+
+void Replace(Hunk *NewHunk, DiffFileNumber ChangedFile, sint32 Offset)
+{
+    // Temporary hunk pointer
+    Hunk *Temp = NewHunk;
+
+    // Do actual replacement in the linked list
+    NewHunk->Prev = Prev;
+    NewHunk->Next = Next;
+    Prev = NULL;
+    Next = NULL;
+
+    if(Offset != 0)
+    {
+        // File must be specified for offset
+        assert(ChangedFile != DiffFile_Unspecified);
+        // Loop through all the hunks after the new one we inserted,
+        while((Temp = NewHunk->Next))
+        {
+            Temp->Start[ChangedFile] += Offset;
+        }
+    }
 }
