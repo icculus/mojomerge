a3fbd40b3391375f1f6918c15654583734c423be
Gregory S. Read <zeph@clutteredmind.org>
2003-06-21 07:02:36 +0000

    -Added "FileThatIsDifferent" parameter to constructor (used for three-way diffs) -GetDiffFile function added -Replace function added

--- Hunk.h
+++ Hunk.h
@@ -35,11 +35,16 @@ namespace MojoMerge
          *          set to UNSPECIFIED then Start3 is treated as an "insert
          *          after" line number.  This parameter is optional when
          *          dealing with a two-way file comparison.
+         *      FileThatIsDifferent
+         *          Indicates which file in a three-way diff is different
+         *          for the specified hunk.  If UNSPECIFIED, that indicates
+         *          that all files are different for that particular hunk.
          *  Returns
          *      none
          */
         Hunk(Hunk *Prev, uint32 Start1, uint32 End1, uint32 Start2, uint32 End2,
-            uint32 Start3 = UNSPECIFIED, uint32 End3 = UNSPECIFIED);
+            uint32 Start3 = UNSPECIFIED, uint32 End3 = UNSPECIFIED, 
+            DiffFileNumber FileThatIsDifferent = DiffFile_Unspecified);
 
         /*  Hunk Destructor
          *  Params
@@ -69,6 +74,17 @@ namespace MojoMerge
          */
         uint32 GetEnd(DiffFileNumber FileNumber);
 
+        /*  GetDiffFile
+         *      Gets the file that is different in a three-way diff
+         *  Params
+         *      none
+         *  Returns
+         *      File number that is different from the other two.  If all
+         *      three files are different, or it's a two-way diff, then
+         *      the return value is UNSPECIFIED
+         */
+        DiffFileNumber GetDiffFile();
+
         /*  GetPrevHunk and GetNextHunk
          *      Returns the previous or next hunk in the linked list respectively.
          *  Parameters
@@ -97,15 +113,38 @@ namespace MojoMerge
          *      none
          */
         void DeleteList();
-    private:
-        #define MAX_DIFF_FILES  3
 
+        /*  Replace
+         *      Removes the hunk from the list and replaces it with the
+         *      specified hunk.  If we are not part of a list, this function
+         *      has no affect.
+         *  Params
+         *      NewHunk
+         *          Hunk that will replace this hunk.  It is assumed that this
+         *          hunk is not part of another list.  This value can be NULL
+         *          which indicates that the hunk should not be replaced with
+         *          a new one.
+         *      ChangedFile
+         *          File that got changed in the old hunk that created the new hunk
+         *      Offset
+         *          Number to offset all hunks that occur after the one being
+         *          replaced.  This is useful if the new hunk is a different
+         *          size than the original one, therefore inadvertantly
+         *          shifting all hunks after it.  0 is also valid to indicate
+         *          no offset.
+         *  Returns
+         *      none
+         */
+        void Replace(Hunk *NewHunk, DiffFileNumber ChangedFile, sint32 Offset);
+    private:
         // Pointers to previous and next hunk in the list
         Hunk *Prev;
         Hunk *Next;
         // Line numbers associated with hunk
         uint32 Start[MAX_DIFF_FILES];
         uint32 End[MAX_DIFF_FILES];
+        // File that is different in a three-way diff
+        DiffFileNumber FileThatIsDifferent;
     };
 }
 
