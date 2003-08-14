e7adcb7aff9a89928bb24caebe6813ff375dea55
Gregory S. Read <18175637+readgs@users.noreply.github.com>
2003-08-14 16:13:10 +0000

    -Text editing control derived from wxStyledTextCtrl. -Helper functions were added to make marking diffs and other stuff easier.

---
+++ DiffTextEdit.cpp
@@ -0,0 +1,114 @@
+/*  DiffTextEdit.cpp - Implementation of DiffTextEdit class
+ *
+ */
+
+#include "DiffTextEdit.h"
+#include "Application.h"
+
+using namespace MojoMerge;
+
+#define STYLE_CHANGE    1
+//#define STYLE_INSERT    2
+
+DiffTextEdit::DiffTextEdit(wxWindow *Parent, wxWindowID id) :
+wxStyledTextCtrl(Parent, id, wxDefaultPosition, wxSize(80,40), wxSUNKEN_BORDER)
+{
+    // Default font to use for text
+    wxFont DefaultFont(8, wxMODERN, wxNORMAL, wxNORMAL);
+    wxColour DefaultChangeColor(138, 170, 189);
+
+    SetScrollWidth(200);
+    StyleSetFont(wxSTC_STYLE_DEFAULT, DefaultFont);
+    // Set styles for the change highlighting    
+    StyleSetBackground(STYLE_CHANGE, DefaultChangeColor);
+    StyleSetEOLFilled(STYLE_CHANGE, true);
+    SetMarginWidth(0, 32);
+    SetMarginWidth(1, 0);
+    // Set styles for the insert after highlighting
+    //StyleSetUnderline(STYLE_INSERT, true);
+    //StyleSetEOLFilled(STYLE_INSERT, true);
+    //SetViewEOL(true);
+}
+
+void DiffTextEdit::MarkChangedLine(int Start, int End)
+{
+    int StartPos, EndPos;       // Start and end position to highlight
+    int LineLength;             // Length of line in text control
+
+    // Get actual positions in editor window of lines
+    StartPos = PositionFromLine(Start - 1);
+    // Get length of end line
+    LineLength = this->LineLength(End - 1);
+    // Get last position of end line
+    EndPos = PositionFromLine(End - 1) + LineLength;
+    // Highlight the change text appropriately
+    StartStyling(StartPos, 0x1F);
+    SetStyling(EndPos - StartPos, STYLE_CHANGE);
+    // If we styled to the end of file
+    if(End == GetLineCount())
+        EOFStyleBugHack();
+}
+
+void DiffTextEdit::MarkInsertAfterLine(int Line)
+{
+/*    EndPos = FileTextEdit->GetLineEndPosition(Start - 1);
+    // Highlight the change text appropriately
+    FileTextEdit->StartStyling(StartPos, 0x1F);
+    FileTextEdit->SetStyling(EndPos - StartPos + 2, STYLE_INSERT);*/
+}
+
+void DiffTextEdit::MarkClearAll()
+{
+    int Pos;    // Temporary position variable
+
+    // Get last position in file
+    Pos = GetLineEndPosition(GetLineCount() - 1);
+    // Start styling with default style
+    StartStyling(0, 0x1F);
+    // Style the whole file
+    SetStyling(Pos, 0);
+    // We're styling to EOF, call the bug fix function
+    EOFStyleBugHack();
+}
+
+void DiffTextEdit::EOFStyleBugHack()
+{
+    // TODO - Should fix scintilla with this bug
+    // Add text and remove text from the last line to get
+    //  around a seemingly strange bug in scintilla.  If we
+    //  don't do this, the style does not get applied to the
+    //  last line (i.e. anything without an EOL char).  But
+    //  this is only true if the cursor is located on the
+    //  last line as well.  Moving the cursor programmatically
+    //  doesn't seem to fix it, but moving the cursor manually does
+    //  fix the problem.
+    int Pos;
+
+    // Get last position in entire file
+    Pos = GetLineEndPosition(GetLineCount() - 1);
+    // Add an "x" character to the end
+    InsertText(Pos, "x");
+    // Remove the "x".  This allows the style to refresh correctly.
+    SetTargetStart(Pos);
+    SetTargetEnd(Pos + 1);
+    ReplaceTarget("");
+}
+
+void DiffTextEdit::DisplayText(const wxString& text)
+{
+    ClearAll();
+    ClearDocumentStyle();
+    SetText(text);
+}
+
+void DiffTextEdit::ConfigSetFont(wxFont &NewFont)
+{
+}
+
+void DiffTextEdit::ConfigSetShowEOL(bool ShowEOL)
+{
+}
+
+void DiffTextEdit::ConfigSetChangeColor(wxColour &Foreground, wxColour &Background)
+{
+}
---
+++ DiffTextEdit.h
@@ -0,0 +1,90 @@
+/*  DiffTextEdit.h - wxStyledTextCtrl with special "helper" functions used
+ *  specifically for marking diffs, merging, etc...
+ */
+
+#ifndef _DIFFTEXTEDIT_H_
+#define _DIFFTEXTEDIT_H_
+
+#include "wx/stc/stc.h"
+
+namespace MojoMerge
+{
+    class DiffTextEdit : public wxStyledTextCtrl
+    {
+        //DECLARE_EVENT_TABLE()
+    public:
+        /*  DiffTextEdit
+         *      Constructs a new DiffTextEdit object
+         *  Params
+         *      Parent
+         *          Window that his control is added to
+         *      id
+         *          ID associated with control
+         *  Returns
+         *      none
+         */
+        DiffTextEdit(wxWindow *Parent, wxWindowID id);
+
+        /*  MarkChangedLine
+         *      Highlights the specified line range with the color used for
+         *      marking chnages.
+         *  Params
+         *      Start
+         *          Starting line of range (starting at line 1)
+         *      End
+         *          Endling line of range
+         *  Returns
+         *      none
+         */
+        void MarkChangedLine(int Start, int End);
+
+        /*  MarkInsertAfterLine
+         *      Marks the specified line as an "insert after" line
+         *  Params
+         *      Line
+         *          Line to change (starting at line 1)
+         *  Returns
+         *      none
+         */
+        void MarkInsertAfterLine(int Line);
+
+        /*  MarkClearAll
+         *      Clear all marks made in text
+         *  Params
+         *      none
+         *  Returns
+         *      none
+         */
+        void MarkClearAll();
+
+        /*  DisplayText
+         *      Just like the wxStyledTextCtrl::SetText except this one also
+         *      does some resetting of styles
+         *  Params
+         *      See wxStyledTextCtrl::SetText control for more information
+         *  Returns
+         *      none
+         */
+        void DisplayText(const wxString& text);
+
+        /*  EOFStyleBugHack
+         *      Work around for a styling bug in the wxStyledTextCtrl.  See the
+         *      function implementation for specifics.
+         *  Params
+         *      none
+         *  Returns
+         *      none
+         */
+        void EOFStyleBugHack();
+
+        void ConfigSetFont(wxFont &NewFont);
+        void ConfigSetShowEOL(bool ShowEOL);
+        void ConfigSetChangeColor(wxColour &Foreground, wxColour &Background);
+
+        // Event handlers
+        //void OnPainted(wxStyledTextEvent& event);
+    private:
+	};
+}
+
+#endif
