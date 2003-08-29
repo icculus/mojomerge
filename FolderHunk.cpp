0afc14efbd5754de2476d6bdf2975e6ded9b5746
Gregory S. Read <18175637+readgs@users.noreply.github.com>
2003-08-29 18:49:13 +0000

    -Made some minor enhancements -Lots of bug fixes

--- FolderHunk.cpp
+++ FolderHunk.cpp
@@ -4,13 +4,16 @@
 
 #include "FolderHunk.h"
 #include <assert.h>
+#include <string.h>
 
 using namespace MojoMerge;
 
-FolderHunk::FolderHunk(FolderHunk *Prev, const char *Path, bool IsFolder,
+FolderHunk::FolderHunk(FolderHunk *Prev, const char *Path,
+    const char *BarePath, bool IsFolder,
     Hunk *FileHunk, FolderHunk *FirstChild, bool ExistsFolder1,
     bool ExistsFolder2, bool ExistsFolder3)
 {
+    LinkHead = false;
     // Set our previous hunk
     this->Prev = Prev;
     // If previous is a valid object
@@ -22,17 +25,27 @@ FolderHunk::FolderHunk(FolderHunk *Prev, const char *Path, bool IsFolder,
     // Path can't be NULL
     assert(Path);
     // Save the passed arguments to our member variables
-    this->Path = Path;
+    strcpy(this->Path, Path);
+    strcpy(this->BarePath, BarePath);
     this->IsFolder = IsFolder;
-    this->FileHunk = FileHunk;
-    this->FirstChild = NULL;
+    //this->FileHunk = FileHunk;
+    this->FirstChild = FirstChild;
     Exists[DiffFile_One] = ExistsFolder1;
     Exists[DiffFile_Two] = ExistsFolder2;
     Exists[DiffFile_Three] = ExistsFolder3;    
 }
 
+FolderHunk::FolderHunk(FolderHunk *FirstHunk)
+{
+    LinkHead = true;
+    Next = FirstHunk;
+    Prev = NULL;
+    FirstChild = NULL;
+}
+
 bool FolderHunk::GetExists(DiffFileNumber FolderNumber)
 {
+    assert(!LinkHead);
     return Exists[FolderNumber];
 }
 
@@ -43,6 +56,7 @@ FolderHunk *FolderHunk::GetFirstChild()
 
 FolderHunk *FolderHunk::GetPrevious()
 {
+    assert(!LinkHead);
     return Prev;
 }
 
@@ -53,10 +67,29 @@ FolderHunk *FolderHunk::GetNext()
 
 const char *FolderHunk::GetPath()
 {
+    assert(!LinkHead);
     return Path;
+}
+
+const char *FolderHunk::GetBarePath()
+{
+    assert(!LinkHead);
+    return BarePath;
 }
 
 bool FolderHunk::GetIsFolder()
 {
+    assert(!LinkHead);
     return IsFolder;
+}
+
+void FolderHunk::DeleteList()
+{
+    // Delete next hunk if applicable
+    if(this->FirstChild)
+        this->FirstChild->DeleteList();
+    if(this->Next)
+        this->Next->DeleteList();
+    // Delete this hunk...duh.
+    delete this;
 }
